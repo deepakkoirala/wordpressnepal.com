79270c05d7b3a983b5b0fcd84ddb3cb818ee9544
Deepak Koirala <dksofts@gmail.com>
2018-10-13 22:55:54 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<title>WordPressNepal.com</title>
+<style>
+body {
+display:flex;
+background:#F9F9FA;
+justify-content: center;
+height:70vh;
+align-items: center;
+}
+strong {
+text-align:center;
+font-family: tahoma;
+}
+span {
+    margin-top:10%;
+    color:#aaa;
+    text-transform:uppercase;
+    font-size:10px;
+    display:block;
+    font-weight:normal;
+}
+a {
+    color:#bbb;
+}
+</style>
+<strong><img src="wordpress-logo.svg"><br>************** WordPressNepal.com - Nepal ************** <br>
+<br>info@wordpressnepal.com
+<br>
+<span>
+    <a href="https://github.com/deepakkoirala/wordpressnepal.com">CONTRIBUTE</a><br><br>
+    Domain for Sale.</span>
+</strong>
